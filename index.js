03948b797f1d5f796586dd7cb48bb778f6c6a6c4
Pub <96320073+b3Rhunter@users.noreply.github.com>
2023-03-07 16:06:34 -0600

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const messages = [
 const { Server } = require("socket.io");
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000"
+    origin: "https://pubcord-lovat.vercel.app/"
   }
 })
 
